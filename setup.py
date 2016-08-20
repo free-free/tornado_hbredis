f3a1ac890fe2a80448afea1b1bd09de8a346236e
John <19941222hb@gmail.com>
2016-08-21 00:02:43 +0800

    change setup options

--- setup.py
+++ setup.py
@@ -2,17 +2,37 @@
 
 from setuptools import setup,find_packages
 
-DESCRIPTION = "tornado_hbredis is an async redis client for tornado,it'a actually encapsulation fro tornadis"
+import os
+
+
+def read(f):
+    open(os.path.join(os.path.dirname(__file__),f)).read().strip()
+
+
+DESCRIPTION = "tornado_hbredis is an asynchronous redis client for tornado"
 
 setup(
     name="tornado_hbredis",
-    version="0.0.1",
-    author="HUANGBIAO",
+    version=0.1,
+    author="HuangBiao",
     author_email="19941222hb@gmail.com",
-    url="https://github.com/free-free/tornado_hbredis",
-    packages=find_packages(),
-    LICENSE="MIT",
-    download_url="https;//github.com/free-free/tornado_hbredis",
     description=DESCRIPTION,
-    install_requires=["tornado","tornadis"]
+    long_description=read("README.rst"),
+    license="MIT",
+    url="https://github.com/free-free/tornado_hbredis",
+    download_url="https;//github.com/free-free/tornado_hbredis",
+    packages=find_packages(),
+    install_requires=["tornado","tornadis"],
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
+    ]
 )
